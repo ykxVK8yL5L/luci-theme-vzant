2e856695f0394972942874c3a8ab96787c030c01
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2021-04-15 08:51:52 +0800

    Update vzanty.lua

--- luci-theme-vzant/luasrc/controller/admin/vzanty.lua
+++ luci-theme-vzant/luasrc/controller/admin/vzanty.lua
@@ -1,12 +1,37 @@
 module("luci.controller.admin.vzanty", package.seeall)
 
 function index()
-    entry({"admin", "status"}, alias("admin", "status", "vzant"), _("Status"), 0).index = true
-    entry({"admin", "status", "vzant"}, template("themes/vzant/vzant"), _("Vzant"), 0)
+    
+
+    if not nixio.fs.access("/etc/config/vzant") then return end
+	if nixio.fs.access("/etc/config/vzant_show") then
+		entry({"admin", "status"}, alias("admin", "status", "vzant"), _("Status"), 0).index = true
+		entry({"admin", "status", "vzant"}, template("themes/vzant/vzant"), _("Vzant"), 0)
+	else
+		entry({"admin", "status"}, alias("admin", "status", "overview"), _("Status"), 20).index = true
+		entry({"admin", "status", "vzant"}, template("themes/vzant/vzant"), _("Vzant"), 60)
+	end
+	entry({"admin", "status", "vzant", "show"}, call("show_menu")).leaf = true
+	entry({"admin", "status", "vzant", "hide"}, call("hide_menu")).leaf = true
+
     entry({'admin', 'status', 'vzant', 'addfavourite'}, call('addfavourite'))
     entry({'admin', 'status', 'vzant', 'delfavourite'}, call('delfavourite'))
     entry({'admin', 'status', 'vzant', 'getfavourites'}, call('getfavourites'))
 end
+
+
+
+function show_menu()
+	luci.sys.call("touch /etc/config/vzant_show")
+	luci.http.redirect(luci.dispatcher.build_url("admin", "status", "vzant"))
+end
+
+function hide_menu()
+	luci.sys.call("rm -rf /etc/config/vzant_show")
+	luci.http.redirect(luci.dispatcher.build_url("admin", "status", "overview"))
+end
+
+
 
 function addfavourite()
   local title = string.gsub(luci.http.formvalue("title"), "%s+", "")
